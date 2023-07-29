9e16b83f15089f1227ddf1beb19efb3bc9e60874
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-07-29 16:50:23 +0700

    feat: add detail trx to trx

--- internal/daos/detail_trx.go
+++ internal/daos/detail_trx.go
@@ -5,14 +5,15 @@ import (
 )
 
 type DetailTrx struct {
-	ID          int64 `gorm:"primaryKey;autoIncrement:true"`
-	IdTrx       int64
-	Trx         Trx `gorm:"foreignKey:IdTrx"`
-	IdLogProduk int64
-	LogProduk   LogProduk `gorm:"foreignKey:IdLogProduk"`
-	IdToko      int64
-	Kuantitas   int
-	HargaTotal  int
-	UpdatedAt   time.Time `gorm:"type:TIMESTAMP;default:CURRENT_TIMESTAMP()"`
-	CreatedAt   time.Time `gorm:"type:TIMESTAMP;default:CURRENT_TIMESTAMP()"`
+	ID         int64 `gorm:"primaryKey;autoIncrement:true"`
+	IdTrx      int64
+	Trx        Trx `gorm:"foreignKey:IdTrx"`
+	IdProduk   int64
+	Produk     Produk `gorm:"foreignKey:IdProduk"`
+	IdToko     int64
+	Toko       Toko `gorm:"foreignKey:IdToko"`
+	Kuantitas  int
+	HargaTotal int
+	UpdatedAt  time.Time `gorm:"type:TIMESTAMP;default:CURRENT_TIMESTAMP()"`
+	CreatedAt  time.Time `gorm:"type:TIMESTAMP;default:CURRENT_TIMESTAMP()"`
 }
--- internal/daos/trx_daos.go
+++ internal/daos/trx_daos.go
@@ -12,10 +12,13 @@ type (
 		IdAlamat    int64
 		Alamat      Alamat `gorm:"foreignKey:IdAlamat"`
 		HargaTotal  int64
-		KodeInvoice string    `gorm:"size:255;index:idx_kodeinvoice,unique"`
-		MethodBayar string    `gorm:"size:255"`
-		UpdatedAt   time.Time `gorm:"type:TIMESTAMP;default:CURRENT_TIMESTAMP()"`
-		CreatedAt   time.Time `gorm:"type:TIMESTAMP;default:CURRENT_TIMESTAMP()"`
+		KodeInvoice string `gorm:"size:255;index:idx_kodeinvoice,unique"`
+		MethodBayar string `gorm:"size:255"`
+		IdToko      int64
+		Toko        Toko        `gorm:"foreignKey:IdToko"`
+		DetailTrxes []DetailTrx `gorm:"foreignKey:IdTrx"`
+		UpdatedAt   time.Time   `gorm:"type:TIMESTAMP;default:CURRENT_TIMESTAMP()"`
+		CreatedAt   time.Time   `gorm:"type:TIMESTAMP;default:CURRENT_TIMESTAMP()"`
 	}
 
 	FilterTrx struct {
